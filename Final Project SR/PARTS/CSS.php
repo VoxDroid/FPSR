46c1e0a75bf4238f78e5c52661c6804c1af2769c
Izeno <jandreijandrei103@gmail.com>
2024-05-24 15:29:15 +0800

    upd

--- Final Project SR/PARTS/CSS.php
+++ Final Project SR/PARTS/CSS.php
@@ -9,7 +9,7 @@ if ($current_page === 'index.php') {
     <link href="ASSETS/IMG/EMS_Icons/EMS_Icon.png" rel="apple-touch-icon">
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="ASSETS/FONTS/Poppins/Poppins.css" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
     <link href="CSS/bootstrap.min.css" rel="stylesheet">
@@ -27,7 +27,7 @@ HTML;
     <link href="../ASSETS/IMG/EMS_Icons/EMS_Icon.png" rel="apple-touch-icon">
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="../ASSETS/FONTS/Poppins/Poppins.css" rel="stylesheet">
 
     <!-- UPPER Bootstrap CSS -->
     <link href="../CSS/bootstrap.min.css" rel="stylesheet">
